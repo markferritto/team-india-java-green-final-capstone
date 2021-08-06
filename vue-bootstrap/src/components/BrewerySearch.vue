230e837d31ded7160c2aaaa623d64caf9efc5407
Anthony Gautier <gautier626@gmail.com>
2021-08-06 10:25:56 -0400

    That was intense

--- vue-bootstrap/src/components/BrewerySearch.vue
+++ vue-bootstrap/src/components/BrewerySearch.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div class="container">
     <div class="row">
       <div
@@ -36,6 +37,45 @@
       </div>
     </div>
   </div>
+  
+  <div class="openContainer">
+    <div class="row">
+      <div
+        class="col-sm-4 py-3"
+        v-for="brewery in openBreweries"
+        v-on:click="sendToOpenBrewery(brewery.breweryId)"
+        v-bind:key="brewery.breweryId"
+      >
+        <div class="card card-body h-100" style="width: 18rem">
+          <img class="card-img-top" src="/img/bg1.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">{{ brewery.name }}</h5>
+            <p class="card-text description">
+              {{ brewery.description }}
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ brewery.brewery_type }}</li>
+            <li class="list-group-item">
+              {{ brewery.street }}. {{ brewery.city }},
+              {{ brewery.state }}
+              {{ brewery.postalCode }}
+            </li>
+            <li class="list-group-item">
+              <a href="#">
+                <div class="stars">
+                  <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i> <i class="fa fa-star"></i>
+                  {{ brewery.rating }}
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -48,6 +88,7 @@ export default {
       breweryFilter: "",
       breweries: [],
       isLoading: true,
+      openBreweries: [],
     };
   },
   computed: {
@@ -63,13 +104,45 @@ export default {
     sendToBrewery(breweryId) {
       this.$router.push(`/brewery/${breweryId}`);
     },
+    sendToOpenBrewery(website_url) {
+      this.$router.push(`${website_url}`);
+    }
   },
   created() {
     breweryService.retrieve().then((response) => {
       this.breweries = response.data;
       this.isLoading = false;
     });
+    fetch('https://api.openbrewerydb.org/breweries?by_state=ohio&by_city=columbus&per_page=50')
+  .then((response) => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then((data) => {
+        this.brewery = data;
+        this.breweryList = JSON.parse(JSON.stringify(data));
+        this.breweryList.forEach(brewery => {
+        this.breweryId = brewery.id;
+        this.name = brewery.name;
+        this.typeName = brewery.brewery_type;
+        this.streetAddress = brewery.street;
+        this.city = brewery.city;
+        this.state = brewery.state;
+        this.zip = brewery.zip;
+        this.websiteUrl = brewery.website_url;
+        this.openBreweries.push(brewery);
+        });
+      });
   },
+  )
+  .catch((err) => {
+    console.log('Fetch Error', err);
+  });
+  }
 };
 </script>
 

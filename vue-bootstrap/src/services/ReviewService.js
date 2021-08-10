8a1abb3eae74c2de79f483fe5fb187fce54cfc0d
John Hurdle <john.h95@gmail.com>
2021-08-09 23:49:58 -0400

    Pushing work

--- vue-bootstrap/src/services/ReviewService.js
+++ vue-bootstrap/src/services/ReviewService.js
@@ -2,20 +2,18 @@ import axios from 'axios';
 
 export default {
 
-    retrieveBreweryReviews(review) {
-        return axios.post('reviews/breweries/{id}', review)
-    },
-    retrieveBeerReviews(review) {
-        return axios.post('/reviews/breweries/{id}/beer/{beerId}', review)
-    },
-    addReview(id,review) {
-        return axios.post(`/reviews/new/breweries/${id}`,review)
-    },
-    addReviews(beerReview) {
-        return axios.post('/reviews/new/breweries/{id}/beer/{beerId}', review)
-    },
     reviewList(id){
         return axios.get(`reviews/breweries/${id}`);
-    }
+    },
+    retrieveBeerReviews(id) {
+        return axios.post(`/reviews/breweries/${id}/beer`)
+    },
+    addReview(id, review) {
+        return axios.post(`/reviews/new/breweries/${id}`, review)
+    },
+    addReviews(id, beerId, review) {
+        return axios.post(`/reviews/new/breweries/${id}/beer/${beerId}`, review)
+    },
+
 
 }
